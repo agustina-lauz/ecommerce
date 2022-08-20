ee405c059096aef2ec52913da7c62c02da602b01
agustinalauz <agustinalauz81@gmail.com>
2022-08-20 00:07:32 -0300

    listado_productos
    Se agrega código de js para mostrar el listado de autos y se crea login con redirección al main

--- js/login.js
+++ js/login.js
@@ -1,20 +1,32 @@
 
 
-let email = document.getElementById("email")
-let password = document.getElementById("pwd")
+let email = document.getElementById("email");
+let password = document.getElementById("pwd");
+let button = document.getElementById("logBtn");
+let emailError = "Ingresa tu email";
+let passError = "Ingresa tu contraseña";
 
 function validateData(){
     return email.value.length > 0 && password.value.length > 0;
 }
 
+function redirect(){
+    location.href = "main.html";
+}
+
+function alertData(){
+    
+}
+
+
 button.addEventListener("click", e =>
     {
-        if (validateData()) {
-            location.href="main.html";
-        } else {
-            console.log("error");
-        }
+      if (validateData()) {
+       redirect();
+      } else {
+        alertData();
+      }
 
-       
-}
+    }
+
  )
---
+++ js/products.js
@@ -0,0 +1,53 @@
+
+const CARS_LIST = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+let productsArray = [];
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let products = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ products.name + " " + " - " + products.currency + " " + products.cost +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cars").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+/* 
+EJECUCIÓN:
+
+-Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
+-Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CARS_LIST).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            currentAutosArray = productsArray.products;
+            showProductsList(currentAutosArray);
+        }
+    });
+});
